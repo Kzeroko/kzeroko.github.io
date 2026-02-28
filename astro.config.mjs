a5efe2bbfddfd1a34ac1b27343cb25133a1058c9
Kzeroko <sa20000906@gmail.com>
2026-02-28 17:08:01 +0800

    Try
    meh

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://kentomahou.com',
 	integrations: [
 		starlight({
 			title: 'My Docs',
