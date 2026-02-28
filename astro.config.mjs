dc5551d71458f575412b64b80943cb41866e69a5
Kzeroko <sa20000906@gmail.com>
2026-02-28 17:47:19 +0800

    Revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kzeroko.github.io',
-	base: '/master',
 	integrations: [
 		starlight({
 			title: 'My Docs',
